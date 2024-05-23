70e19a0fb5a458bb66a41b996d2102c1ab37aaa7
sjoleee <cent7425@gmail.com>
2024-05-24 04:43:32 +0900

    style: Badge 스타일 수정

--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -1,12 +1,11 @@
 import { HTMLAttributes } from "react";
-
 import cn from "@/utils/cn";
 
 const Badge = ({ className, children, ...restProps }: HTMLAttributes<HTMLDivElement>) => {
   return (
     <div
       className={cn(
-        "flex justify-center items-center text-xs rounded-full w-fit px-2 h-5 text-textColor shrink-0 border-textColor border-[0.5px]",
+        "flex justify-center items-center text-xs rounded-full w-fit px-2 h-5 text-textColor shrink-0 bg-textColor/10",
         className,
       )}
       {...restProps}
