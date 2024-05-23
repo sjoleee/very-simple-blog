a96be3540d1075f2a484577fdda971321f44a746
sjoleee <cent7425@gmail.com>
2024-05-24 04:05:20 +0900

    style: mdx break-words 적용

--- src/components/Mdx/index.tsx
+++ src/components/Mdx/index.tsx
@@ -15,7 +15,7 @@ const Mdx = ({ code, className }: MdxProps) => {
   const Component = useMDXComponent(code);
 
   return (
-    <article className="prose dark:prose-invert min-w-full break-keep">
+    <article className="prose dark:prose-invert min-w-full break-words">
       <Component className={className} components={components} />
     </article>
   );
