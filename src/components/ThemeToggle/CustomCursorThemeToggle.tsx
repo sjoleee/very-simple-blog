2734d54809615569ef986e9465a8d09f9008242c
sjoleee <cent7425@gmail.com>
2024-06-09 19:09:07 +0900

    fix: CustomCursorThemeToggle의 popover 위치가 스크롤에 따라 이상하게 렌더되는 버그 수정

--- src/components/ThemeToggle/CustomCursorThemeToggle.tsx
+++ src/components/ThemeToggle/CustomCursorThemeToggle.tsx
@@ -1,7 +1,6 @@
-import { MouseEvent, useEffect, useRef, useState } from "react";
-
 import { ThemeToggleProps } from "./ThemeToggle";
 
+import { MouseEvent, useEffect, useRef, useState } from "react";
 import useTheme from "@/hooks/useTheme";
 import cn from "@/utils/cn";
 import { AnimatePresence, MotionProps, m, useMotionValue } from "framer-motion";
@@ -36,9 +35,9 @@ const CustomCursorThemeToggle = ({
   const handleMouseMove = (e: MouseEvent<HTMLButtonElement>) => {
     if (rect) {
       const scrollX = window.scrollX;
-      const scrollY = window.scrollY;
+
       x.set(e.clientX - rect.left + scrollX);
-      y.set(e.clientY - rect.top + scrollY + 20);
+      y.set(e.clientY - rect.top + 20);
     }
   };
   const handleMouseLeave = () => {
